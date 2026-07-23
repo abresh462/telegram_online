6f43cedd1191aafa50988a8916291755bd8dd892
abresh462 <abrhamberhan118@gmail.com>
2026-07-23 17:57:38 +0300

    Update online.php

--- online.php
+++ online.php
@@ -1,9 +1,27 @@
+<?php
+
+if (!file_exists('madeline.php')) {
+    copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
+}
+include 'madeline.php';
+
+// Read credentials from environment variables
+$api_id = getenv('API_ID');
+$api_hash = getenv('API_HASH');
+$phone_number = getenv('PHONE_NUMBER');
+
+// Create settings with your credentials
 $settings = [
     'app_info' => [
-        'api_id' => getenv('API_ID'),
-        'api_hash' => getenv('API_HASH')
-    ]
+        'api_id' => (int)$api_id,
+        'api_hash' => $api_hash,
+    ],
 ];
 
 $MadelineProto = new \danog\MadelineProto\API('session.madeline', $settings);
 $MadelineProto->start();
+
+while(1) {
+    $MadelineProto->account->updateStatus(offline: false);
+    sleep(5);
+}
